e1db1ac84be459f776a08281a4e1a36062f8c8b9
ivanci0 <unbaigorria.ivan@gmail.com>
2016-11-12 23:08:16 -0300

    Cambios en el menu y tamaño de pantalla
    - Creacion de menu
    - Corregido el tamaño de la camara

--- source/MenuState.hx
+++ source/MenuState.hx
@@ -9,13 +9,20 @@ import flixel.math.FlxMath;
 
 class MenuState extends FlxState
 {
+	var botonJugar:FlxButton;
 	override public function create():Void
 	{
 		super.create();
+		botonJugar = new FlxButton(0, 0, "Jugar", clickJugar);
+		botonJugar.screenCenter();
+		add(botonJugar);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+	}
+	function clickJugar():Void{
+		FlxG.switchState(new PlayState());
 	}
 }
